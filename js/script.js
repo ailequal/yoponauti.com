0b7b8f7c97d0923756b26701989f72279d570f82
ailequal <37016865+ailequal@users.noreply.github.com>
2020-04-11 18:57:53 +0200

    Working on modal window

--- js/script.js
+++ js/script.js
@@ -1,21 +1,48 @@
-// counter
-const counters = document.querySelectorAll('.counter');
-const speed = 500;
+$(document).ready(function () {
+	// counter
+	const counters = document.querySelectorAll('.counter');
+	const speed = 500;
 
-counters.forEach(counter => {
-	const updateCount = () => {
-		const target = +counter.getAttribute('data-target');
-		const count = +counter.innerText;
+	counters.forEach(counter => {
+		const updateCount = () => {
+			const target = +counter.getAttribute('data-target');
+			const count = +counter.innerText;
 
-		const inc = target / speed;
+			const inc = target / speed;
 
-		if (count < target) {
-			counter.innerText = Math.ceil(count + inc);
-			setTimeout(updateCount, 1);
-		} else {
-			count.innerText = target;
+			if (count < target) {
+				counter.innerText = Math.ceil(count + inc);
+				setTimeout(updateCount, 1);
+			} else {
+				count.innerText = target;
+			}
 		}
-	}
 
-	updateCount();
-})
+		updateCount();
+	})
+
+	// modal
+	// when user click on an image
+	$('.item img').on('click', function () {
+		// show that specific modal window
+		let modal = $(this).siblings('.modal');
+		modal.show();
+	});
+
+	// when user click on close
+	$('.close').on('click', function () {
+		// close that specific modal window
+		let modal = $(this).closest('.modal');
+		modal.hide();
+	})
+
+	// handlebars
+	var source = document.getElementById("template").innerHTML;
+	var template = Handlebars.compile(source);
+	var context = {
+		title: "My handlebars post",
+		body: "This is my first post!"
+	};
+	var html = template(context);
+	$('.bars').append(html);
+});
